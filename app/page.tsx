4399fd12fba1b918d3984cecaed7ecea59af5f35
leonel <leonelyimga@gmail.com>
2024-03-07 17:13:26 +0100

    KK

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,108 @@
+"use client";
+import im1 from "@/public/im1.jpg";
+import im2 from "@/public/im2.jpg";
+import im3 from "@/public/im3.jpg";
+import im4 from "@/public/im4.jpg";
+import im5 from "@/public/im5.jpg";
+import gsap from "gsap";
 import Image from "next/image";
-
+import { useEffect, useRef } from "react";
 export default function Home() {
+  const im1a = useRef(null);
+  const im2a = useRef(null);
+  const im3a = useRef(null);
+  const im4a = useRef(null);
+  const im5a = useRef(null);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    tl.to(im1a.current, {
+      y: -400,
+      x: 200,
+      opacity: 1,
+      ease: "power2.inOut",
+    })
+      .to([im2a.current, im3a.current, im4a.current, im5a.current], {
+        y: 330,
+        stagger: {
+          each: 0.2,
+        },
+        duration: 0.8,
+        ease: "expo.inOut",
+      })
+      .to(im1a.current, {
+        // x: 100,
+        scale: 1.5,
+      });
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col overflow-hidden items-center bg-[#F988BE] justify-between ">
+      <div className="relative flex gap-x-8 h-screen w-full items-end  justify-center">
+        <div
+          ref={im1a}
+          className="w-[247px] h-[247px] relative  left-0 bg-red-500"
+        >
+          <Image
+            src={im1}
+            height={247}
+            className="object-cover h-full w-full relative "
+            width={247}
+            alt=""
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          ref={im2a}
+          className="w-[247px] h-[247px] relative  left-0 bg-red-500"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Image
+            src={im2}
+            height={247}
+            className="object-cover relative"
+            width={247}
+            alt=""
+          />
+        </div>
+        <div
+          ref={im3a}
+          className="w-[247px] h-[247px] relative  left-0 bg-red-500"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Image
+            ref={im3a}
+            src={im3}
+            height={247}
+            className="object-cover relative"
+            width={247}
+            alt=""
+          />
+        </div>
+        <div
+          ref={im4a}
+          className="w-[247px] h-[247px] relative  left-0 bg-red-500"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <Image
+            ref={im4a}
+            src={im4}
+            height={247}
+            className="object-cover relative"
+            width={247}
+            alt=""
+          />
+        </div>
+        <div
+          ref={im5a}
+          className="w-[247px] h-[247px] relative  left-0 bg-red-500"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <Image
+            ref={im5a}
+            src={im5}
+            height={247}
+            className="object-cover relative"
+            width={247}
+            alt=""
+          />
+        </div>
       </div>
     </main>
   );
